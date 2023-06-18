7fa8b623501c79d9c8247c7c03ef6e4a091172cb
craigSochocki <sochocki.craig@gmail.com>
2023-06-18 09:56:35 -0400

    add messagebubble component

--- src/components/chatbox.tsx
+++ src/components/chatbox.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import MessageBubble from './messagebubble'
+
 
 export default function chatbox() {
+    const handleSend = () => {
+        console.log('im working')
+    }
+
   return (
     <div>
       <div className="flex h-screen antialiased text-gray-800">
@@ -141,38 +147,7 @@ export default function chatbox() {
           <div className="flex flex-col h-full overflow-x-auto mb-4">
             <div className="flex flex-col h-full">
               <div className="grid grid-cols-12 gap-y-2">
-                <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                  <div className="flex flex-row items-center">
-                    <div
-                      className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
-                    >
-                      A
-                    </div>
-                    <div
-                      className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
-                    >
-                      <div>Hey How are you today?</div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                  <div className="flex flex-row items-center">
-                    <div
-                      className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
-                    >
-                      A
-                    </div>
-                    <div
-                      className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
-                    >
-                      <div>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Vel ipsa commodi illum saepe numquam maxime
-                        asperiores voluptate sit, minima perspiciatis.
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <MessageBubble />
                 <div className="col-start-6 col-end-13 p-3 rounded-lg">
                   <div className="flex items-center justify-start flex-row-reverse">
                     <div
@@ -238,93 +213,9 @@ export default function chatbox() {
                     </div>
                   </div>
                 </div>
+                <MessageBubble />
                 <div className="col-start-1 col-end-8 p-3 rounded-lg">
                   <div className="flex flex-row items-center">
-                    <div
-                      className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
-                    >
-                      A
-                    </div>
-                    <div
-                      className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
-                    >
-                      <div>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Perspiciatis, in.
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-start-1 col-end-8 p-3 rounded-lg">
-                  <div className="flex flex-row items-center">
-                    <div
-                      className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
-                    >
-                      A
-                    </div>
-                    <div
-                      className="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
-                    >
-                      <div className="flex flex-row items-center">
-                        <button
-                          className="flex items-center justify-center bg-indigo-600 hover:bg-indigo-800 rounded-full h-8 w-10"
-                        >
-                          <svg
-                            className="w-6 h-6 text-white"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
-                            ></path>
-                            <path
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                          </svg>
-                        </button>
-                        <div className="flex flex-row items-center space-x-px ml-4">
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-12 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-6 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-5 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-3 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-10 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-1 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-1 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-8 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-2 w-1 bg-gray-500 rounded-lg"></div>
-                          <div className="h-4 w-1 bg-gray-500 rounded-lg"></div>
-                        </div>
-                      </div>
-                    </div>
                   </div>
                 </div>
               </div>
@@ -381,6 +272,7 @@ export default function chatbox() {
             </div>
             <div className="ml-4">
               <button
+              onClick={handleSend}
                 className="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
               >
                 <span>Send</span>
